25538e3cea7ff55eda605b959d0232b481307f44
Bradley-Krigbaum <bradley.krigbaum@gmail.com>
2020-05-01 19:05:58 -0700

    adding files to repo

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import axios from "axios";
+import axiosWithAuth from "../utils/axiosWithAuth";
 
 const initialColor = {
   color: "",
@@ -7,7 +7,6 @@ const initialColor = {
 };
 
 const ColorList = ({ colors, updateColors }) => {
-  console.log(colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
 
@@ -20,11 +19,29 @@ const ColorList = ({ colors, updateColors }) => {
     e.preventDefault();
     // Make a put request to save your updated color
     // think about where will you get the id from...
-    // where is is saved right now?
+    // where is it saved right now?
+
+    axiosWithAuth()
+      .put(`http://localhost:5000/api/colors/${colorToEdit.id}`, 
+        { ...colorToEdit, color: colorToEdit.color, code: colorToEdit.code })
+      .then(res => {
+        console.log('bk: ColorList.js: saveEdit: response: ', res)
+        updateColors(res)
+      })
+      .catch(err => console.log('bk: ColorList.js: saveEdit: error: ', err));
+
   };
 
   const deleteColor = color => {
-    // make a delete request to delete this color
+
+    // console.log('color.id: ', color.id)
+    axiosWithAuth()
+      .delete(`http://localhost:5000/api/colors/${color.id}`)
+      .then(res => {
+        console.log('bk: ColorList.js: deleteColor: response: ', res)
+        window.location.reload();
+      })
+      .catch(err => console.log('bk: ColorList.js: deleteColor: error: ', err))
   };
 
   return (
